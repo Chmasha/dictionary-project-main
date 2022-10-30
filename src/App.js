687f079b39a44108de99821eca96ff6f86b6bb2b
Chmasha <chorna.mariia@gmail.com>
2022-10-30 11:00:28 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Created by Mariia Chorna</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
